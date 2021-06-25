fa31f20f8a21a2f1ef894a6bc5f6bcb5c52c1abb
Pineapplelab17 <pineapplelab17@gmail.com>
2021-06-25 14:54:03 -0300

    ADD: Course / Course Category Bloc

---
+++ lib/blocs/course_bloc.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:oluko_app/models/assessment.dart';
+import 'package:oluko_app/models/course.dart';
+import 'package:oluko_app/repositories/course_repository.dart';
+
+abstract class CourseState {}
+
+class CourseLoading extends CourseState {}
+
+class CourseSuccess extends CourseState {
+  final List<Course> values;
+  CourseSuccess({this.values});
+}
+
+class CourseFailure extends CourseState {
+  final Exception exception;
+
+  CourseFailure({this.exception});
+}
+
+class CourseBloc extends Cubit<CourseState> {
+  CourseBloc() : super(CourseLoading());
+
+  void get() async {
+    if (!(state is CourseSuccess)) {
+      emit(CourseLoading());
+    }
+    try {
+      List<Course> courses = await CourseRepository().getAll();
+      emit(CourseSuccess(values: courses));
+    } catch (e) {
+      emit(CourseFailure(exception: e));
+    }
+  }
+
+  void getForAssessment(Assessment assessment) async {
+    if (!(state is CourseSuccess)) {
+      emit(CourseLoading());
+    }
+    try {
+      List<Course> courses = await CourseRepository().getAll();
+      emit(CourseSuccess(values: courses));
+    } catch (e) {
+      emit(CourseFailure(exception: e));
+    }
+  }
+}
---
+++ lib/blocs/course_category_bloc.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:oluko_app/models/assessment.dart';
+import 'package:oluko_app/models/course_category.dart';
+import 'package:oluko_app/repositories/course_category_repository.dart';
+
+abstract class CourseCategoryState {}
+
+class CourseCategoryLoading extends CourseCategoryState {}
+
+class CourseCategorySuccess extends CourseCategoryState {
+  final List<CourseCategory> values;
+  CourseCategorySuccess({this.values});
+}
+
+class CourseCategoryFailure extends CourseCategoryState {
+  final Exception exception;
+
+  CourseCategoryFailure({this.exception});
+}
+
+class CourseCategoryBloc extends Cubit<CourseCategoryState> {
+  CourseCategoryBloc() : super(CourseCategoryLoading());
+
+  void get() async {
+    if (!(state is CourseCategorySuccess)) {
+      emit(CourseCategoryLoading());
+    }
+    try {
+      List<CourseCategory> courses = await CourseCategoryRepository().getAll();
+      emit(CourseCategorySuccess(values: courses));
+    } catch (e) {
+      emit(CourseCategoryFailure(exception: e));
+    }
+  }
+
+  void getForAssessment(Assessment assessment) async {
+    if (!(state is CourseCategorySuccess)) {
+      emit(CourseCategoryLoading());
+    }
+    try {
+      List<CourseCategory> courses = await CourseCategoryRepository().getAll();
+      emit(CourseCategorySuccess(values: courses));
+    } catch (e) {
+      emit(CourseCategoryFailure(exception: e));
+    }
+  }
+}
