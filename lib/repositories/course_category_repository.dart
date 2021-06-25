bf505cc00754f204a7e9354686d6133ac8b99516
Pineapplelab17 <pineapplelab17@gmail.com>
2021-06-25 14:47:00 -0300

    ADD: Couse and CourseCategory repositories

---
+++ lib/repositories/course_category_repository.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:oluko_app/models/course_category.dart';
+
+class CourseCategoryRepository {
+  FirebaseFirestore firestoreInstance;
+
+  CourseCategoryRepository() {
+    firestoreInstance = FirebaseFirestore.instance;
+  }
+
+  CourseCategoryRepository.test({FirebaseFirestore firestoreInstance}) {
+    this.firestoreInstance = firestoreInstance;
+  }
+
+  Future<List<CourseCategory>> getAll() async {
+    QuerySnapshot docRef = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc(GlobalConfiguration().getValue("projectId"))
+        .collection('courseCategories')
+        .get();
+    List<CourseCategory> response = [];
+    docRef.docs.forEach((doc) {
+      final Map<String, dynamic> element = doc.data();
+      response.add(CourseCategory.fromJson(element));
+    });
+    return response;
+  }
+}
