1eda3c1fbdcc36c6c9a03d5ca5ebaf1213412cdf
Pineapplelab17 <pineapplelab17@gmail.com>
2021-06-29 17:38:58 -0300

    CHANGE: Extend course & courseCategory from Base

--- lib/models/course.dart
+++ lib/models/course.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'base.dart';
 
-class Course {
+class Course extends Base {
   Course(
       {this.name,
       this.id,
@@ -15,7 +16,16 @@ class Course {
       this.engagementTime,
       this.mandatoryGapTime,
       this.classe,
-      this.imageUrl});
+      this.imageUrl,
+      Timestamp createdAt,
+      String createdBy,
+      Timestamp updatedAt,
+      String updatedBy})
+      : super(
+            createdBy: createdBy,
+            createdAt: createdAt,
+            updatedAt: updatedAt,
+            updatedBy: updatedBy);
 
   String name;
   String id;
@@ -32,23 +42,31 @@ class Course {
   bool mandatoryGapTime;
   List<Map<String, dynamic>> classe;
   String imageUrl;
+  Timestamp createdAt;
+  String createdBy;
+  Timestamp updatedAt;
+  String updatedBy;
 
   Course.fromJson(Map json)
-      : name = json['name'] ?? null,
-        id = json['id'] ?? null,
-        duration = json['duration'] ?? null,
-        description = json['description'] ?? null,
-        equipment = json['equipment'] ?? null,
-        intensity = json['intensity'] ?? null,
-        categories = json['categories'] ?? null,
-        workoutDuration = json['workout_duration'] ?? null,
-        recommendedEngagement = json['recommended_engagement'] ?? null,
-        recommendedEngagementGap = json['recommended_engagementGap'] ?? null,
-        engagementGapTime = json['engagement_gap_time'] ?? null,
-        engagementTime = json['engagement_time'] ?? null,
-        mandatoryGapTime = json['mandatory_gap_time'] ?? null,
-        classe = json['classe'] ?? null,
-        imageUrl = json['image_url' ?? null];
+      : name = json['name'],
+        id = json['id'],
+        duration = json['duration'],
+        description = json['description'],
+        equipment = json['equipment'],
+        intensity = json['intensity'],
+        categories = json['categories'],
+        workoutDuration = json['workout_duration'],
+        recommendedEngagement = json['recommended_engagement'],
+        recommendedEngagementGap = json['recommended_engagementGap'],
+        engagementGapTime = json['engagement_gap_time'],
+        engagementTime = json['engagement_time'],
+        mandatoryGapTime = json['mandatory_gap_time'],
+        classe = json['classe'],
+        imageUrl = json['image_url'],
+        createdAt = json['created_at'],
+        createdBy = json['created_by'],
+        updatedAt = json['updated_at'],
+        updatedBy = json['updated_by'];
 
   Map<String, dynamic> toJson() => {
         'name': name,
@@ -65,5 +83,9 @@ class Course {
         'mandatory_gap_time': mandatoryGapTime,
         'classe': classe,
         'image_url': imageUrl,
+        'created_at': createdAt == null ? createdAtSentinel : createdAt,
+        'created_by': createdBy,
+        'updated_at': updatedAt == null ? updatedAtSentinel : updatedAt,
+        'updated_by': updatedBy,
       };
 }
--- lib/models/course_category.dart
+++ lib/models/course_category.dart
@@ -1,26 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:oluko_app/models/base.dart';
 import 'package:oluko_app/models/course_category_item.dart';
 
-class CourseCategory {
-  CourseCategory({this.name, this.id, this.courses});
+class CourseCategory extends Base {
+  CourseCategory(
+      {this.name,
+      this.id,
+      this.courses,
+      Timestamp createdAt,
+      String createdBy,
+      Timestamp updatedAt,
+      String updatedBy})
+      : super(
+            createdBy: createdBy,
+            createdAt: createdAt,
+            updatedAt: updatedAt,
+            updatedBy: updatedBy);
 
   String name;
   String id;
   List<CourseCategoryItem> courses;
+  Timestamp createdAt;
+  String createdBy;
+  Timestamp updatedAt;
+  String updatedBy;
 
   CourseCategory.fromJson(Map json)
       : name = json['name'],
         id = json['id'],
-        courses = json['courses'] != null && json['courses'].length > 0
-            ? json['courses'].map<CourseCategoryItem>((item) {
-                CourseCategoryItem courseCategoryItem =
-                    CourseCategoryItem.fromJson(item);
-                return courseCategoryItem;
-              }).toList()
-            : [];
+        courses = json['courses'] != null
+            ? json['courses']
+                .map<CourseCategoryItem>(
+                    (item) => CourseCategoryItem.fromJson(item))
+                .toList()
+            : [],
+        createdAt = json['created_at'],
+        createdBy = json['created_by'],
+        updatedAt = json['updated_at'],
+        updatedBy = json['updated_by'];
 
   Map<String, dynamic> toJson() => {
         'name': name,
         'id': id,
         'courses': courses,
+        'created_at': createdAt == null ? createdAtSentinel : createdAt,
+        'created_by': createdBy,
+        'updated_at': updatedAt == null ? updatedAtSentinel : updatedAt,
+        'updated_by': updatedBy,
       };
 }
