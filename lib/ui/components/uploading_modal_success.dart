f72c33003928129ec8def7183ba86f0709d3b24e
Pineapplelab17 <pineapplelab17@gmail.com>
2021-07-23 15:51:12 -0300

    FIX: 'Parent Data Widget' issue on Transformation Journey success modal

--- lib/ui/components/uploading_modal_success.dart
+++ lib/ui/components/uploading_modal_success.dart
@@ -9,37 +9,47 @@ class UploadingModalSuccess extends StatelessWidget {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: 300,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50.0),
-            child: CircleAvatar(
-              backgroundColor: OlukoColors.primary,
-              radius: 40.0,
-              child: IconButton(
-                  icon: Icon(Icons.check, color: OlukoColors.black),
-                  onPressed: () {}),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: CircleAvatar(
+                    backgroundColor: OlukoColors.primary,
+                    radius: 40.0,
+                    child: IconButton(
+                        icon: Icon(Icons.check, color: OlukoColors.black),
+                        onPressed: () {}),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "Uploaded Successfully",
+                    style: OlukoFonts.olukoTitleFont(
+                        custoFontWeight: FontWeight.w400),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Row(
+                        children: [
+                          OlukoOutlinedButton(
+                            title: "Done",
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ],
+                      )),
+                )
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              "Uploaded Successfully",
-              style:
-                  OlukoFonts.olukoTitleFont(custoFontWeight: FontWeight.w400),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: OlukoOutlinedButton(
-                  title: "Done",
-                  onPressed: () => Navigator.pop(context),
-                )),
-          )
         ],
       ),
     );
