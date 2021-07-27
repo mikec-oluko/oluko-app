6688e38cd5883136c82efd1152d3c82c75b63db1
Pineapplelab17 <pineapplelab17@gmail.com>
2021-07-27 13:54:16 -0300

    ADD: Recommendations repository

---
+++ lib/repositories/recommendation_repository.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:oluko_app/models/recommendation.dart';
+
+class RecommendationRepository {
+  FirebaseFirestore firestoreInstance;
+
+  RecommendationRepository() {
+    firestoreInstance = FirebaseFirestore.instance;
+  }
+
+  RecommendationRepository.test({FirebaseFirestore firestoreInstance}) {
+    this.firestoreInstance = firestoreInstance;
+  }
+
+  Future<List<Recommendation>> getAll() async {
+    QuerySnapshot docRef =
+        await FirebaseFirestore.instance.collection('recommendations').get();
+    List<Recommendation> response = [];
+    docRef.docs.forEach((doc) {
+      final Map<String, dynamic> element = doc.data();
+      response.add(Recommendation.fromJson(element));
+    });
+    return response;
+  }
+
+  Future<List<Recommendation>> getByDestinationUser(userId) async {
+    QuerySnapshot docRef = await FirebaseFirestore.instance
+        .collection('recommendations')
+        .where('destination_user_id', isEqualTo: userId)
+        .get();
+    List<Recommendation> response = [];
+    docRef.docs.forEach((doc) {
+      final Map<String, dynamic> element = doc.data();
+      response.add(Recommendation.fromJson(element));
+    });
+    return response;
+  }
+}
