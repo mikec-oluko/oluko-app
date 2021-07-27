c09f6e439dc0aa7be8d4403fcb6fb9568964d7c9
Pineapplelab17 <pineapplelab17@gmail.com>
2021-07-27 14:33:51 -0300

    FIX: Recommendations Repository - Fetch from project-specific collection

--- lib/repositories/recommendation_repository.dart
+++ lib/repositories/recommendation_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:global_configuration/global_configuration.dart';
 import 'package:oluko_app/models/recommendation.dart';
 
 class RecommendationRepository {
@@ -13,8 +14,11 @@ class RecommendationRepository {
   }
 
   Future<List<Recommendation>> getAll() async {
-    QuerySnapshot docRef =
-        await FirebaseFirestore.instance.collection('recommendations').get();
+    QuerySnapshot docRef = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc(GlobalConfiguration().getValue("projectId"))
+        .collection('recommendations')
+        .get();
     List<Recommendation> response = [];
     docRef.docs.forEach((doc) {
       final Map<String, dynamic> element = doc.data();
@@ -25,6 +29,8 @@ class RecommendationRepository {
 
   Future<List<Recommendation>> getByDestinationUser(userId) async {
     QuerySnapshot docRef = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc(GlobalConfiguration().getValue("projectId"))
         .collection('recommendations')
         .where('destination_user_id', isEqualTo: userId)
         .get();
