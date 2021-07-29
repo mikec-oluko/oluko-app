7215a58e36a6d8b268fa22248d63fe7236f601ba
Pineapplelab17 <pineapplelab17@gmail.com>
2021-07-29 15:27:16 -0300

    ADD: Favorite Repository

---
+++ lib/repositories/favorite_repository.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:oluko_app/models/favorite.dart';
+
+class FavoriteRepository {
+  FirebaseFirestore firestoreInstance;
+
+  FavoriteRepository() {
+    firestoreInstance = FirebaseFirestore.instance;
+  }
+
+  FavoriteRepository.test({FirebaseFirestore firestoreInstance}) {
+    this.firestoreInstance = firestoreInstance;
+  }
+
+  Future<List<Favorite>> getAll() async {
+    QuerySnapshot docRef = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc(GlobalConfiguration().getValue("projectId"))
+        .collection('favorites')
+        .get();
+    List<Favorite> response = [];
+    docRef.docs.forEach((doc) {
+      final Map<String, dynamic> element = doc.data();
+      response.add(Favorite.fromJson(element));
+    });
+    return response;
+  }
+
+  Future<List<Favorite>> getByUserId(userId) async {
+    QuerySnapshot docRef = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc(GlobalConfiguration().getValue("projectId"))
+        .collection('recommendations')
+        .where('user_id', isEqualTo: userId)
+        .get();
+    List<Favorite> response = [];
+    docRef.docs.forEach((doc) {
+      final Map<String, dynamic> element = doc.data();
+      response.add(Favorite.fromJson(element));
+    });
+    return response;
+  }
+}
