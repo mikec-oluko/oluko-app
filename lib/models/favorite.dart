b3994d1eebd1377795d62a81af37c7d3a3868832
Pineapplelab17 <pineapplelab17@gmail.com>
2021-07-29 15:05:36 -0300

    ADD: Favorite model

---
+++ lib/models/favorite.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:oluko_app/models/base.dart';
+import 'package:oluko_app/models/submodels/object_submodel.dart';
+
+class Favorite extends Base {
+  String userId;
+  ObjectSubmodel course;
+
+  Favorite(
+      {this.userId,
+      this.course,
+      String id,
+      Timestamp createdAt,
+      String createdBy,
+      Timestamp updatedAt,
+      String updatedBy,
+      bool isHidden,
+      bool isDeleted})
+      : super(
+            id: id,
+            createdBy: createdBy,
+            createdAt: createdAt,
+            updatedAt: updatedAt,
+            updatedBy: updatedBy,
+            isDeleted: isDeleted,
+            isHidden: isHidden);
+
+  factory Favorite.fromJson(Map<String, dynamic> json) {
+    Favorite favorite = Favorite(
+        userId: json['user_id'],
+        course: json['course'] == null
+            ? null
+            : json['course']
+                .map<ObjectSubmodel>(
+                    (course) => ObjectSubmodel.fromJson(course))
+                .toList());
+    favorite.setBase(json);
+    return favorite;
+  }
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> favoriteJson = {
+      'user_id': userId,
+      'course': course == null ? null : course.toJson(),
+    };
+    favoriteJson.addEntries(super.toJson().entries);
+    return favoriteJson;
+  }
+}
