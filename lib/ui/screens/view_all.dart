a136675521a8d17a16552003fcd87264b46ae844
Pineapplelab17 <pineapplelab17@gmail.com>
2021-08-02 15:38:01 -0300

    ADD: Dynamic title on view all

--- lib/ui/screens/view_all.dart
+++ lib/ui/screens/view_all.dart
@@ -2,12 +2,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:oluko_app/models/course.dart';
 import 'package:oluko_app/models/search_results.dart';
+import 'package:oluko_app/ui/components/black_app_bar.dart';
 import 'package:oluko_app/utils/course_utils.dart';
+import 'package:oluko_app/utils/oluko_localizations.dart';
 
 class ViewAll extends StatefulWidget {
   List<Course> courses;
+  String title;
 
-  ViewAll({this.courses, Key key}) : super(key: key);
+  ViewAll({this.courses, this.title, Key key}) : super(key: key);
 
   @override
   _ViewAllState createState() => _ViewAllState();
@@ -22,6 +25,10 @@ class _ViewAllState extends State<ViewAll> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: OlukoAppBar(
+            title: widget.title != null
+                ? widget.title
+                : OlukoLocalizations.of(context).find('viewAll')),
         backgroundColor: Colors.black,
         body: CourseUtils.searchResults(context,
             SearchResults(searchResults: widget.courses), 0.69333, 3, 5));
