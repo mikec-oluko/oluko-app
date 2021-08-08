b1f16149136003607a8de7bc35254ad982652afc
Joaquin Cayrus <joaquin.cayrus@pineapple-lab.com>
2021-08-08 20:53:16 -0300

    UPDATE: env qa

--- lib/config/project_settings.dart
+++ lib/config/project_settings.dart
@@ -1,5 +1,5 @@
 final Map<String, String> projectSettings = {
   "projectId": "WnZEZDQDT9ZRU2nlpa86", // MVT Gim
   "firebaseFunctions":
-      "https://us-central1-oluko-development.cloudfunctions.net", // DEVELOPMENT
+      "https://us-central1-oluko-qa.cloudfunctions.net", // DEVELOPMENT
 };
