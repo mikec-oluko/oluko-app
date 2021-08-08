ec4763eeb69bb65ce01c141416f7b1cf09a07670
Joaquin Cayrus <joaquin.cayrus@pineapple-lab.com>
2021-08-08 19:10:45 -0300

    UPDATE: config url for functions

--- lib/config/project_settings.dart
+++ lib/config/project_settings.dart
@@ -1,3 +1,5 @@
 final Map<String, String> projectSettings = {
-  "projectId": "WnZEZDQDT9ZRU2nlpa86" // MVT Gim
+  "projectId": "WnZEZDQDT9ZRU2nlpa86", // MVT Gim
+  "firebaseFunctions":
+      "https://us-central1-oluko-development.cloudfunctions.net", // DEVELOPMENT
 };
