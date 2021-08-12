b626ced131a0206b0988c2a6465fb111960e8ca2
Pineapplelab17 <pineapplelab17@gmail.com>
2021-08-12 11:13:22 -0300

    ADD: Move friend request to friend

--- lib/models/friend_model.dart
+++ lib/models/friend_model.dart
@@ -2,40 +2,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:oluko_app/models/base.dart';
 
-class FriendModel extends Base {
+class FriendModel {
   DocumentReference reference;
   bool isMuted;
   DateTime isMutedUntil;
   DateTime latestSeenContent;
+  String id;
 
   FriendModel(
       {this.reference,
       this.isMuted,
       this.isMutedUntil,
       this.latestSeenContent,
-      String id,
-      Timestamp createdAt,
-      String createdBy,
-      Timestamp updatedAt,
-      String updatedBy,
-      bool isHidden,
-      bool isDeleted})
-      : super(
-            id: id,
-            createdBy: createdBy,
-            createdAt: createdAt,
-            updatedAt: updatedAt,
-            updatedBy: updatedBy,
-            isDeleted: isDeleted,
-            isHidden: isHidden);
+      this.id});
 
   factory FriendModel.fromJson(Map<String, dynamic> json) {
     FriendModel favorite = FriendModel(
         reference: json['reference'],
         isMuted: json['is_muted'],
         isMutedUntil: json['is_muted_until'],
-        latestSeenContent: json['latest_seen_content']);
-    favorite.setBase(json);
+        latestSeenContent: json['latest_seen_content'],
+        id: json['id']);
     return favorite;
   }
 
@@ -44,9 +31,9 @@ class FriendModel extends Base {
       'reference': reference,
       'is_muted': isMuted,
       'is_muted_until': isMutedUntil,
-      'latest_seen_content': latestSeenContent
+      'latest_seen_content': latestSeenContent,
+      'id': id
     };
-    favoriteJson.addEntries(super.toJson().entries);
     return favoriteJson;
   }
 }
