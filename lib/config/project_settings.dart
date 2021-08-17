3615fc7df6e8a8fea4379452cd8519cd906a8a23
Joaquin Cayrus <joaquin.cayrus@pineapple-lab.com>
2021-08-17 12:42:49 -0300

    UPDATE: environment update + local build

--- lib/config/project_settings.dart
+++ lib/config/project_settings.dart
@@ -1,8 +1,10 @@
 final Map<String, String> projectSettings = {
   "projectId": "WnZEZDQDT9ZRU2nlpa86", // MVT Gim
   "firebaseFunctions":
-      "https://us-central1-oluko-qa.cloudfunctions.net", // DEVELOPMENT
-  "environment": "quality-assurance",
+      "https://us-central1-oluko-development.cloudfunctions.net", // DEVELOPMENT
+  // "build": "production",
+  "build": "local",
+  "environment": "development",
   "sentryDsn":
       "https://e036e5ce9dbe4bb49bfb4a5fa5b27bb1@o462724.ingest.sentry.io/5900594",
 };
