b39fb730614b91da1926b1733ef5c45eb9981bb1
Victoria Moraes <>
2021-08-18 08:09:18 -0400

    Unnecessary import removed

--- lib/ui/screens/courses/inside_class.dart
+++ lib/ui/screens/courses/inside_class.dart
@@ -1,5 +1,4 @@
 import 'package:chewie/chewie.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
