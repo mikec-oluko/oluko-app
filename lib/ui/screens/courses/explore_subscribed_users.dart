84ed841325d4de989f69e4b7ed0424cb80961fe0
Pineapplelab17 <pineapplelab17@gmail.com>
2021-08-24 14:15:11 -0300

    CHANGE: Remove sample avatar images

--- lib/ui/screens/courses/explore_subscribed_users.dart
+++ lib/ui/screens/courses/explore_subscribed_users.dart
@@ -18,15 +18,6 @@ class ExploreSubscribedUsers extends StatefulWidget {
 
 class _ExploreSubscribedUsersState extends State<ExploreSubscribedUsers> {
   List<UserResponse> allEnrolledUsers;
-  List<String> storiesItem = const [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrpM3UTTyyqIwGsPYB1gCDhfl3XVv0Cex2Lw&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlCzsqcGBluOOUtgQahXtISLTM3Wb2tkpsoeMqwurI2LEP6pCS0ZgCFLQGiv8BtfJ9p2A&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMWzdlSputkYso9dJb4VY5VEWQunXGBJMgGys7BLC4MzPQp6yfLURe-9nEdGrcK6Jasc&usqp=CAU',
-    'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20%2820%29.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHNX4Bb1o5JWY91Db6I4jf_wmw24ajOdaOPgRCqFlnEnxcAlQ42pyWJxM9klp3E8JoT0k&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF-rBV5pmJhYA8QbjpPcx6s9SywnXGbvsaxWyFi47oDf9JuL4GruKBY5zl2tM4tdgYdQ0&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF1L_s4YJh7RHSIag8CxT0LTuJQo-XQnTJkVApDXar4b0A57U_TnAMrK_l4Fd_Nzp65Bg&usqp=CAU'
-  ];
 
   @override
   Widget build(BuildContext context) {
