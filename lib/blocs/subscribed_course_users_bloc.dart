772ed9968dd05213fb246968bb8a69c55aff6ab2
Pineapplelab17 <pineapplelab17@gmail.com>
2021-08-24 15:32:24 -0300

    ADD: Get favorite friends from Explore subscribed users

--- lib/blocs/subscribed_course_users_bloc.dart
+++ lib/blocs/subscribed_course_users_bloc.dart
@@ -4,9 +4,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oluko_app/models/course.dart';
 import 'package:oluko_app/models/course_category.dart';
 import 'package:oluko_app/models/course_enrollment.dart';
+import 'package:oluko_app/models/friend.dart';
+import 'package:oluko_app/models/friend_model.dart';
 import 'package:oluko_app/models/user_response.dart';
 import 'package:oluko_app/repositories/course_enrollment_repository.dart';
 import 'package:oluko_app/repositories/course_repository.dart';
+import 'package:oluko_app/repositories/friend_repository.dart';
 import 'package:oluko_app/repositories/user_repository.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
@@ -16,7 +19,8 @@ class SubscribedCourseUsersLoading extends SubscribedCourseUsersState {}
 
 class SubscribedCourseUsersSuccess extends SubscribedCourseUsersState {
   final List<UserResponse> users;
-  SubscribedCourseUsersSuccess({this.users});
+  final List<UserResponse> favoriteUsers;
+  SubscribedCourseUsersSuccess({this.users, this.favoriteUsers});
 }
 
 class SubscribedCourseUsersFailure extends SubscribedCourseUsersState {
@@ -27,7 +31,7 @@ class SubscribedCourseUsersFailure extends SubscribedCourseUsersState {
 class SubscribedCourseUsersBloc extends Cubit<SubscribedCourseUsersState> {
   SubscribedCourseUsersBloc() : super(SubscribedCourseUsersLoading());
 
-  void get(String courseId) async {
+  void get(String courseId, String userId) async {
     try {
       //Fetch enrollments for this course. And retrieve all users that are already enrolled.
       List<CourseEnrollment> courseEnrollmentList =
@@ -48,7 +52,24 @@ class SubscribedCourseUsersBloc extends Cubit<SubscribedCourseUsersState> {
           uniqueUserList.add(element);
         }
       });
-      emit(SubscribedCourseUsersSuccess(users: uniqueUserList));
+
+      Friend friendData = await FriendRepository.getUserFriendsByUserId(userId);
+      List<FriendModel> friends = friendData.friends;
+
+      List<UserResponse> favoriteUserList = [];
+
+      uniqueUserList.map((user) => {
+            friends.forEach((friend) {
+              if (friend.id == user.id && friend.isFavorite) {
+                uniqueUserList
+                    .removeWhere((element) => element.id == friend.id);
+                favoriteUserList.add(user);
+              }
+            })
+          });
+
+      emit(SubscribedCourseUsersSuccess(
+          users: uniqueUserList, favoriteUsers: favoriteUserList));
     } catch (exception, stackTrace) {
       await Sentry.captureException(
         exception,
--- lib/ui/screens/courses/explore_subscribed_users.dart
+++ lib/ui/screens/courses/explore_subscribed_users.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:oluko_app/blocs/auth_bloc.dart';
 import 'package:oluko_app/blocs/subscribed_course_users_bloc.dart';
 import 'package:oluko_app/models/user_response.dart';
 import 'package:oluko_app/ui/components/black_app_bar.dart';
@@ -18,57 +19,64 @@ class ExploreSubscribedUsers extends StatefulWidget {
 
 class _ExploreSubscribedUsersState extends State<ExploreSubscribedUsers> {
   List<UserResponse> allEnrolledUsers;
+  AuthSuccess loggedUser;
 
   @override
   Widget build(BuildContext context) {
-    if (allEnrolledUsers == null) {
-      BlocProvider.of<SubscribedCourseUsersBloc>(context).get(widget.courseId);
-    }
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: _appBar(),
-      body: Container(
-        height: ScreenUtils.height(context),
-        width: ScreenUtils.width(context),
-        child: ListView(children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              child: BlocBuilder<SubscribedCourseUsersBloc,
-                      SubscribedCourseUsersState>(
-                  builder: (context, subscribedCourseUsersState) {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Row(
-                        children: [
-                          TitleBody('Favourites'),
-                        ],
+    return BlocBuilder<AuthBloc, AuthState>(builder: (context, authState) {
+      if (authState is AuthSuccess &&
+          loggedUser == null &&
+          allEnrolledUsers == null) {
+        loggedUser = authState;
+        BlocProvider.of<SubscribedCourseUsersBloc>(context)
+            .get(widget.courseId, loggedUser.user.id);
+      }
+      return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: _appBar(),
+        body: Container(
+          height: ScreenUtils.height(context),
+          width: ScreenUtils.width(context),
+          child: ListView(children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                child: BlocBuilder<SubscribedCourseUsersBloc,
+                        SubscribedCourseUsersState>(
+                    builder: (context, subscribedCourseUsersState) {
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Row(
+                          children: [
+                            TitleBody('Favourites'),
+                          ],
+                        ),
                       ),
-                    ),
-                    subscribedCourseUsersState is SubscribedCourseUsersSuccess
-                        ? usersGrid(subscribedCourseUsersState.users)
-                        : SizedBox(),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Row(
-                        children: [
-                          TitleBody('Everyone else'),
-                        ],
+                      subscribedCourseUsersState is SubscribedCourseUsersSuccess
+                          ? usersGrid(subscribedCourseUsersState.favoriteUsers)
+                          : SizedBox(),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Row(
+                          children: [
+                            TitleBody('Everyone else'),
+                          ],
+                        ),
                       ),
-                    ),
-                    subscribedCourseUsersState is SubscribedCourseUsersSuccess
-                        ? usersGrid(subscribedCourseUsersState.users)
-                        : SizedBox()
-                  ],
-                );
-              }),
+                      subscribedCourseUsersState is SubscribedCourseUsersSuccess
+                          ? usersGrid(subscribedCourseUsersState.users)
+                          : SizedBox()
+                    ],
+                  );
+                }),
+              ),
             ),
-          ),
-        ]),
-      ),
-    );
+          ]),
+        ),
+      );
+    });
   }
 
   Widget _appBar() {
@@ -80,35 +88,40 @@ class _ExploreSubscribedUsersState extends State<ExploreSubscribedUsers> {
   }
 
   Widget usersGrid(List<UserResponse> users) {
-    return GridView.count(
-        childAspectRatio: 0.8,
-        crossAxisCount: 4,
-        physics: new NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        children: users
-            .map((user) => Column(
-                  children: [
-                    StoriesItem(
-                      maxRadius: 30,
-                      imageUrl: user.avatar != null
-                          ? user.avatar
-                          : 'https://firebasestorage.googleapis.com/v0/b/oluko-development.appspot.com/o/avatar.png?alt=media&token=c16925c3-e2be-47fb-9d15-8cd1469d9790',
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0, bottom: 0.0),
-                      child: Text(
-                        '${user.firstName} ${user.lastName}',
-                        style: TextStyle(color: Colors.white, fontSize: 13),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Text(
-                      '${user.username}',
-                      style: TextStyle(color: Colors.grey, fontSize: 10),
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                ))
-            .toList());
+    return users.length > 0
+        ? GridView.count(
+            childAspectRatio: 0.8,
+            crossAxisCount: 4,
+            physics: new NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: users
+                .map((user) => Column(
+                      children: [
+                        StoriesItem(
+                          maxRadius: 30,
+                          imageUrl: user.avatar != null
+                              ? user.avatar
+                              : 'https://firebasestorage.googleapis.com/v0/b/oluko-development.appspot.com/o/avatar.png?alt=media&token=c16925c3-e2be-47fb-9d15-8cd1469d9790',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0, bottom: 0.0),
+                          child: Text(
+                            '${user.firstName} ${user.lastName}',
+                            style: TextStyle(color: Colors.white, fontSize: 13),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Text(
+                          '${user.username}',
+                          style: TextStyle(color: Colors.grey, fontSize: 10),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ))
+                .toList())
+        : Padding(
+            padding: EdgeInsets.only(bottom: 20, top: 10),
+            child: TitleBody('No Users.'),
+          );
   }
 }
