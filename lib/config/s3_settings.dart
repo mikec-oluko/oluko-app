1f3eb0a1f6cb07f977debae443a0046f0dfa5c4e
Joaquin Cayrus <joaquin.cayrus@pineapple-lab.com>
2021-08-25 16:54:50 -0300

    DEPLOY: updated configs and deploy data

--- lib/config/s3_settings.dart
+++ lib/config/s3_settings.dart
@@ -2,5 +2,5 @@ final Map<String, String> s3Settings = {
   "accessKeyID": "AKIAVMUKSA35X2YF7MFL",
   "secretAccessKey": "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",
   "region": "us-west-1",
-  "bucket": "https://oluko-development.s3.amazonaws.com"
+  "bucket": "https://oluko-qa.s3.amazonaws.com"
 };
