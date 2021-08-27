93d209050846de312c457c3acc34ac98ed0e337b
Pineapplelab17 <pineapplelab17@gmail.com>
2021-08-26 22:39:16 -0300

    CHANGE: Reduce iterators on subscribed course bloc

--- lib/blocs/subscribed_course_users_bloc.dart
+++ lib/blocs/subscribed_course_users_bloc.dart
@@ -32,19 +32,20 @@ class SubscribedCourseUsersBloc extends Cubit<SubscribedCourseUsersState> {
       List<CourseEnrollment> courseEnrollmentList =
           await CourseEnrollmentRepository.getByCourse(courseId);
 
-      List<UserResponse> userList = await Future.wait(courseEnrollmentList
-          .map((e) => UserRepository().getById(e.userReference.id)));
-
-      userList.removeWhere((element) => element == null);
+      //User list for all subscribers of this course.
+      List<UserResponse> usersSubscribedToCourse = await Future.wait(
+          courseEnrollmentList
+              .map((e) => UserRepository().getById(e.userReference.id)));
+      //Remove enrollments without user
+      usersSubscribedToCourse.removeWhere((element) => element == null);
 
       List<UserResponse> uniqueUserList = [];
-      userList.forEach((element) {
-        if (uniqueUserList
-                .map((e) => e.username)
-                .toList()
-                .indexOf(element.username) ==
-            -1) {
-          uniqueUserList.add(element);
+      List<String> uniqueUserIds = [];
+
+      usersSubscribedToCourse.forEach((userSubscribed) {
+        if (uniqueUserIds.indexOf(userSubscribed.id) == -1) {
+          uniqueUserList.add(userSubscribed);
+          uniqueUserIds.add(userSubscribed.id);
         }
       });
 
@@ -54,15 +55,13 @@ class SubscribedCourseUsersBloc extends Cubit<SubscribedCourseUsersState> {
       List<UserResponse> favoriteUserList = [];
       List<UserResponse> userListToShow = List.from(uniqueUserList);
 
-      uniqueUserList.forEach((user) => {
-            friends.forEach((friend) {
-              if (friend.id == user.id && friend.isFavorite) {
-                userListToShow
-                    .removeWhere((element) => element.id == friend.id);
-                favoriteUserList.add(user);
-              }
-            })
-          });
+      friends.forEach((friend) {
+        if (friend.isFavorite) {
+          num index =
+              userListToShow.map((user) => user.id).toList().indexOf(friend.id);
+          favoriteUserList.add(userListToShow[index]);
+        }
+      });
 
       emit(SubscribedCourseUsersSuccess(
           users: userListToShow, favoriteUsers: favoriteUserList));
