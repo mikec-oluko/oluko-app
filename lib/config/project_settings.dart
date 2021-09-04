d425cc03361f0a913587bc16a6d26f0fbb34071b
Joaquin Cayrus <joaquin.cayrus@pineapple-lab.com>
2021-09-03 21:23:54 -0300

    FIX: deploy config files

--- lib/config/project_settings.dart
+++ lib/config/project_settings.dart
@@ -1,7 +1,7 @@
 final Map<String, String> projectSettings = {
   "projectId": "WnZEZDQDT9ZRU2nlpa86", // MVT Gim
   "firebaseFunctions":
-      "https://us-central1-oluko-development.cloudfunctions.net", // DEVELOPMENT
+      "https://us-central1-oluko-2671e.cloudfunctions.net", // DEVELOPMENT
   "build": "production",
   "environment": "production",
   "sentryDsn":
