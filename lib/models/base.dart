b93eca2844365b1b15648e5055c711d090a5d91e
Joaquin Cayrus <joaquin.cayrus@pineapple-lab.com>
2021-09-17 21:13:31 -0300

    UPDATE: added user dto to save on device

--- lib/models/base.dart
+++ lib/models/base.dart
@@ -1,14 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Base {
-  Base(
-      {this.id,
-      this.createdAt,
-      this.createdBy,
-      this.updatedAt,
-      this.updatedBy,
-      this.isDeleted,
-      this.isHidden}) {
+  Base({this.id, this.createdAt, this.createdBy, this.updatedAt, this.updatedBy, this.isDeleted, this.isHidden}) {
     if (this.createdAt == null) {
       this.createdAtSentinel = FieldValue.serverTimestamp();
     }
@@ -40,11 +33,23 @@ class Base {
     id = json['id'].toString();
     createdAt = json['created_at'] is FieldValue
         ? null
-        : json['created_at'] is Timestamp ? json['created_at'] as Timestamp : json['created_at'] is Map ? Timestamp(json['created_at']['_seconds'] as int, json['created_at']['_nanoseconds'] as int) : null;
+        : json['created_at'] is Timestamp
+            ? json['created_at'] as Timestamp
+            : json['created_at'] is Map
+                ? Timestamp(json['created_at']['_seconds'] as int, json['created_at']['_nanoseconds'] as int)
+                : json['created_at'] is int
+                    ? Timestamp.fromMillisecondsSinceEpoch(json['created_at'] as int)
+                    : null;
     createdBy = json['created_by'].toString();
     updatedAt = json['updated_at'] is FieldValue
         ? null
-        : json['updated_at'] is Timestamp ? json['updated_at'] as Timestamp : json['updated_at'] is Map ? Timestamp(json['updated_at']['_seconds'] as int, json['updated_at']['_nanoseconds'] as int) : null;
+        : json['updated_at'] is Timestamp
+            ? json['updated_at'] as Timestamp
+            : json['updated_at'] is Map
+                ? Timestamp(json['updated_at']['_seconds'] as int, json['updated_at']['_nanoseconds'] as int)
+                : json['updated_at'] is int
+                    ? Timestamp.fromMillisecondsSinceEpoch(json['updated_at'] as int)
+                    : null;
     updatedBy = json['updated_by'].toString();
     isDeleted = json['is_deleted'] as bool;
     isHidden = json['is_hidden'] as bool;
---
+++ lib/models/dto/user_dto.dart
@@ -0,0 +1,129 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:oluko_app/models/user_response.dart';
+
+class UserDto {
+  UserDto.fromUserResponse(UserResponse userReponse) {
+    createdAt = userReponse.createdAt?.seconds;
+    updatedAt = userReponse.updatedAt?.seconds;
+    firstName = userReponse.firstName;
+    lastName = userReponse.lastName;
+    email = userReponse.email;
+    username = userReponse.username;
+    firebaseId = userReponse.firebaseId;
+    hubspotCompanyId = userReponse.hubspotCompanyId;
+    hubspotContactId = userReponse.hubspotContactId;
+    privacy = userReponse.privacy;
+    notification = userReponse.notification;
+    avatar = userReponse.avatar;
+    avatarThumbnail = userReponse.avatarThumbnail;
+    coverImage = userReponse.coverImage;
+    city = userReponse.city;
+    state = userReponse.state;
+    country = userReponse.country;
+    currentPlan = userReponse.currentPlan;
+    id = userReponse.id;
+    createdBy = userReponse.createdBy;
+    updatedBy = userReponse.updatedBy;
+    isHidden = userReponse.isHidden;
+    isDeleted = userReponse.isDeleted;
+  }
+
+  UserDto({
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.username,
+    this.firebaseId,
+    this.hubspotCompanyId,
+    this.hubspotContactId,
+    this.privacy,
+    this.notification,
+    this.avatar,
+    this.avatarThumbnail,
+    this.coverImage,
+    this.city,
+    this.state,
+    this.country,
+    this.currentPlan,
+    this.id,
+    Timestamp createdAt,
+    this.createdBy,
+    Timestamp updatedAt,
+    this.updatedBy,
+    this.isHidden,
+    this.isDeleted,
+  }) {
+    this.createdAt = createdAt?.millisecondsSinceEpoch;
+    this.updatedAt = updatedAt?.millisecondsSinceEpoch;
+  }
+
+  String firstName, lastName, email, username, firebaseId, avatar, avatarThumbnail, coverImage, city, state, country;
+  double currentPlan;
+  num hubspotCompanyId;
+  num hubspotContactId;
+  int privacy;
+  bool notification;
+  String id;
+  int createdAt;
+  String createdBy;
+  int updatedAt;
+  String updatedBy;
+  bool isDeleted = false;
+  bool isHidden = false;
+
+  factory UserDto.fromJson(Map<String, dynamic> json) {
+    UserDto userDto = UserDto(
+      firstName: json['first_name'].toString(),
+      lastName: json['last_name'].toString(),
+      email: json['email'].toString(),
+      username: json['username'].toString(),
+      avatar: json['avatar'].toString(),
+      avatarThumbnail: json['avatar_thumbnail'].toString(),
+      coverImage: json['cover_image'].toString(),
+      city: json['city'].toString(),
+      state: json['state'].toString(),
+      country: json['country'].toString(),
+      firebaseId: json['firebase_id'].toString(),
+      hubspotCompanyId: json['hubspot_company_id'] as num,
+      hubspotContactId: json['hubspot_contact_id'] as num,
+      notification: json['notification'] == null ? true : json['notification'] as bool,
+      privacy: json['privacy'] == null ? 0 : json['privacy'] as int,
+      currentPlan: json['current_plan'] == null ? 0 : double.tryParse((json['current_plan'] as num).toString()),
+      id: json['id'].toString(),
+      createdAt: json['created_at'] as Timestamp,
+      createdBy: json['created_by'].toString(),
+      updatedAt: json['updated_at'] as Timestamp,
+      updatedBy: json['updated_by'].toString(),
+      isDeleted: json['is_deleted'] as bool,
+      isHidden: json['is_hidden'] as bool,
+    );
+    return userDto;
+  }
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> userDtoJson = {
+      'first_name': firstName,
+      'last_name': lastName,
+      'email': email,
+      'username': username,
+      'avatar': avatar,
+      'avatar_thumbnail': avatarThumbnail,
+      'cover_image': coverImage,
+      'city': city,
+      'state': state,
+      'country': country,
+      'firebase_id': firebaseId,
+      'hubspot_company_id': hubspotCompanyId,
+      'hubspot_contact_id': hubspotContactId,
+      'notification': notification ?? true,
+      'privacy': privacy ?? 0,
+      'current_plan': currentPlan,
+      'updated_by': updatedBy,
+      'created_by': createdBy,
+      'id': id,
+      'is_deleted': isDeleted,
+      'is_hidden': isHidden,
+    };
+    return userDtoJson;
+  }
+}
