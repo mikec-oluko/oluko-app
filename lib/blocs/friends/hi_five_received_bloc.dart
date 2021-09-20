c9844eb094073b53cf11054cc60ee79eba00daea
pineapplelab17 <pineapplelab17@gmail.com>
2021-09-19 22:32:34 -0300

    ADD: Hive Five received bloc

--- lib/blocs/friends/hi_five_received_bloc.dart
+++ lib/blocs/friends/hi_five_received_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oluko_app/models/chat.dart';
+import 'package:oluko_app/models/message.dart';
 import 'package:oluko_app/repositories/chat_repository.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
@@ -9,8 +10,8 @@ abstract class HiFiveReceivedState {}
 class HiFiveReceivedLoading extends HiFiveReceivedState {}
 
 class HiFiveReceivedSuccess extends HiFiveReceivedState {
-  List<Chat> chat;
-  HiFiveReceivedSuccess({this.chat});
+  bool hiFive;
+  HiFiveReceivedSuccess({this.hiFive});
 }
 
 class HiFiveReceivedFailure extends HiFiveReceivedState {
@@ -22,11 +23,17 @@ class HiFiveReceivedFailure extends HiFiveReceivedState {
 class HiFiveReceivedBloc extends Cubit<HiFiveReceivedState> {
   HiFiveReceivedBloc() : super(HiFiveReceivedLoading());
 
-  void get(BuildContext context, String userId) async {
+  void get(BuildContext context, String userId, String targetUserId) async {
     try {
-      //Get chat and message info from Chat repository
-      List<Chat> chat = await ChatRepository().getByUserId(userId);
-      emit(HiFiveReceivedSuccess(chat: chat));
+      List<Message> messages =
+          await ChatRepository().getMessages(userId, targetUserId);
+
+      bool hiFive = false;
+      messages.forEach((Message message) =>
+          message.message == Message().hifiveMessageCode
+              ? hiFive = true
+              : hiFive = false);
+      emit(HiFiveReceivedSuccess(hiFive: hiFive));
     } catch (exception, stackTrace) {
       await Sentry.captureException(
         exception,
