15bcf282e064a67d2e089f58eb9dc8ec7a51e13b
Joaquin Cayrus <joaquin.cayrus@pineapple-lab.com>
2021-09-29 22:48:10 -0300

    UPDATE: models from json

--- lib/models/dto/api_response.dart
+++ lib/models/dto/api_response.dart
@@ -7,9 +7,9 @@ class ApiResponse {
   dynamic data;
 
   ApiResponse.fromJson(Map json)
-      : statusCode = num.tryParse(json['statusCode'].toString()),
-        message = json['message'].toString(),
-        error = json['error'].toString(),
+      : statusCode = num.tryParse(json['statusCode']?.toString()),
+        message = json['message']?.toString(),
+        error = json['error']?.toString(),
         data = json['data'];
 
   Map<String, dynamic> toJson() => {'statusCode': statusCode, 'message': message, 'error': error, 'data': data};
