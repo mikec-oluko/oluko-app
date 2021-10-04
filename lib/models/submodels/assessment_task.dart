236d9be1b6af41c102687bc072023fd42c5523f7
Facundo.p <57139117+pineapplelab23@users.noreply.github.com>
2021-10-04 17:30:55 -0300

    UPDATED: Removed index from AssessmentTask submodel

--- lib/models/submodels/assessment_task.dart
+++ lib/models/submodels/assessment_task.dart
@@ -1,16 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AssessmentTask {
-  AssessmentTask({this.taskReference, this.index, this.taskId});
+  AssessmentTask({this.taskReference, this.taskId});
 
   DocumentReference taskReference;
-  num index;
   String taskId;
   String taskName;
 
   AssessmentTask.fromJson(Map json)
       : taskReference = json['reference'] as DocumentReference,
-        index = json['index'] as num,
         taskId = json['id']?.toString(),
         taskName = json['name']?.toString();
 
