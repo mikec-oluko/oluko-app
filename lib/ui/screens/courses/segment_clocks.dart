748db2ef6529e051e6249c84f68fe5b43cc5b70e
jacoboFederico <federico@pineapple-lab.com>
2021-11-11 16:34:21 -0300

    UPDATE OM-1080: Notify in a snack bar the creation of a story

--- lib/ui/screens/courses/segment_clocks.dart
+++ lib/ui/screens/courses/segment_clocks.dart
@@ -981,6 +981,7 @@ class _SegmentClocksState extends State<SegmentClocks> {
     _wantsToCreateStory = true;
     if (_isVideoUploaded) {
       callBlocToCreateStory(context, _segmentSubmission);
+      AppMessages.showSnackbarTranslated(context, 'storyCreated');
     }
   }
 
