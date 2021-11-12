fb566ed7050f63a31cc652b18d58332626b38fe9
jacoboFederico <federico@pineapple-lab.com>
2021-11-12 10:56:22 -0300

    FIX 492: Fix permissions from gallery for ios devices with ios version greater than 14

--- lib/helpers/permissions.dart
+++ lib/helpers/permissions.dart
@@ -1,15 +1,24 @@
 import 'package:oluko_app/helpers/enum_collection.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
 class Permissions {
   static Future<bool> requiredPermissionsEnabled(DeviceContentFrom uploadedFrom) async {
-    if (uploadedFrom == DeviceContentFrom.gallery) {
-      if (await Permission.storage.status.isDenied || await Permission.storage.status.isPermanentlyDenied) {
-        return false;
-      }
-    } else if (uploadedFrom == DeviceContentFrom.camera) {
+    if (uploadedFrom == DeviceContentFrom.camera) {
       if (await Permission.camera.status.isDenied || await Permission.camera.status.isPermanentlyDenied) {
         return false;
+      }
+    } else if (uploadedFrom == DeviceContentFrom.gallery) {
+      if (Platform.isIOS) {
+       if((Platform.version?.split(' ')[0] as double) > 14){
+         if (await Permission.photos.status.isDenied || await Permission.photos.status.isPermanentlyDenied) {
+          return false;
+        }
+       }
+      }else{
+        if (await Permission.storage.status.isDenied || await Permission.storage.status.isPermanentlyDenied) {
+          return false;
+        }
       }
     }
     return true;
