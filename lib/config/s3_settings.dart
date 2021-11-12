54972ced24decb904f0927552517cbfa865f0c39
Joaquin Cayrus <jcayrus@gmail.com>
2021-11-12 12:48:57 -0300

    DEPLOY: 66 & 94

--- lib/config/s3_settings.dart
+++ lib/config/s3_settings.dart
@@ -1,4 +1,4 @@
-final Map<String, String> s3Settings = {
++DEPLOY:final Map<String, String> s3Settings = {
   "accessKeyID": "AKIAVMUKSA35X2YF7MFL",
   "secretAccessKey": "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",
   "region": "us-west-1",
