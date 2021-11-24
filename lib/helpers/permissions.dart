addedd29d8d927b3d1e3c6f9508606fd13d6615f
Joaquin Cayrus <jcayrus@gmail.com>
2021-11-24 15:10:59 -0300

    FIX; permission request

--- lib/helpers/permissions.dart
+++ lib/helpers/permissions.dart
@@ -4,13 +4,18 @@ import 'dart:io';
 
 class Permissions {
   static Future<bool> requiredPermissionsEnabled(DeviceContentFrom uploadedFrom, {bool checkMicrophone = true}) async {
+    if (checkMicrophone) {
+      await Permission.microphone.request();
+    }
     if (uploadedFrom == DeviceContentFrom.camera) {
+      await Permission.camera.request();
       if (await Permission.camera.status.isDenied ||
           await Permission.camera.status.isPermanentlyDenied ||
           checkMicrophone && (await Permission.microphone.status.isDenied || await Permission.microphone.status.isPermanentlyDenied)) {
         return false;
       }
     } else if (uploadedFrom == DeviceContentFrom.gallery) {
+      await Permission.storage.request();
       if (Platform.isIOS) {
         if (double.tryParse(Platform.operatingSystemVersion?.split('.')[0]) >= 14) {
           if (await Permission.photos.status.isDenied || await Permission.photos.status.isPermanentlyDenied) {
