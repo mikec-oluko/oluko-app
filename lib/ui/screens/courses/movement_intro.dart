0b948fe72d25d1360464ff2f157070e218650c40
santiago <santiago@pineapple-lab.com>
2022-02-08 11:42:56 -0300

    FIX when course is null throws redscreen

--- lib/ui/screens/courses/movement_intro.dart
+++ lib/ui/screens/courses/movement_intro.dart
@@ -231,7 +231,10 @@ class _MovementIntroState extends State<MovementIntro> with TickerProviderStateM
                                     child: GestureDetector(
                                       onTap: () => Navigator.pop(context),
                                       child: OlukoBlurredButton(
-                                        childContent: Image.asset('assets/courses/left_back_arrow.png',scale: 3.5,),
+                                        childContent: Image.asset(
+                                          'assets/courses/left_back_arrow.png',
+                                          scale: 3.5,
+                                        ),
                                       ),
                                     ),
                                   ),
@@ -500,7 +503,7 @@ class _MovementIntroState extends State<MovementIntro> with TickerProviderStateM
                           children: _movementInfoSuccess.relatedCourses
                               .map((Course course) => Padding(
                                     padding: const EdgeInsets.only(right: 20.0),
-                                    child: neumorphicCourseRow(course),
+                                    child: course != null ? neumorphicCourseRow(course) : SizedBox(),
                                   ))
                               .toList()),
                     )
