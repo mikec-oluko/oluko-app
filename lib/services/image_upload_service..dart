9e192e1c29d4ffc0ba11ee1a12678fcc5719320d
santiago <santiago@pineapple-lab.com>
2022-04-14 16:17:58 -0300

    FIX OM-1996

---
+++ lib/services/image_upload_service..dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:oluko_app/helpers/s3_provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:path/path.dart' as p;
+
+class ImageUploadService{
+    static Future<String> uploadImageToStorage(String filePath, String path) async {
+    final file = File(filePath);
+    final basename = p.basename(filePath);
+    try {
+      final Reference fileReference =
+          FirebaseStorage.instance.ref('${path}/${basename}');
+      await fileReference.putFile(file);
+      final downloadUrl = await fileReference.getDownloadURL();
+      return downloadUrl;
+    } on Exception catch (e, stackTrace) {
+      await Sentry.captureException(
+        e,
+        stackTrace: stackTrace,
+      );
+      rethrow;
+    }
+  }
+   static Future<String> uploadImageToS3(String filePath, String folderName) async {
+    final file = File(filePath);
+    final basename = p.basename(filePath);
+
+    final S3Provider s3Provider = S3Provider();
+    final String downloadUrl = await s3Provider.putFile(file.readAsBytesSync(), folderName, basename);
+
+    return downloadUrl;
+  }
+}
