1ab29017ce1fce0d71baa6309ece70f383a3eb49
Facundo.p <57139117+pineapplelab23@users.noreply.github.com>
2022-04-19 17:36:00 -0300

    UPDATED: scale updated

--- lib/ui/components/clock.dart
+++ lib/ui/components/clock.dart
@@ -446,8 +446,8 @@ class _State extends State<Clock> {
   Widget roundTimerWithPulse(bool keyboardVisibilty) {
     return Stack(alignment: Alignment.center, children: [
       Transform.scale(
-          scale: 2.5,
-          child: AvatarGlow(glowColor: OlukoNeumorphismColors.olukoNeumorphicGreenWatchColor, endRadius: 100, child: SizedBox.shrink())),
+          scale: 1.3,
+          child: AvatarGlow(glowColor: OlukoNeumorphismColors.olukoNeumorphicGreenWatchColor, endRadius: 150, child: SizedBox.shrink())),
       getRoundsTimer(keyboardVisibilty)
     ]);
   }
