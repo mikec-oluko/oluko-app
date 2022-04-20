d6b5221ddcb6be3fcb81c09cbca41b56a61938c1
Facundo.p <57139117+pineapplelab23@users.noreply.github.com>
2022-04-20 15:22:10 -0300

    UPDATED: Fixed ARMRAP rounds value issues
    -Updated logic for ARMRAP rounds, on save
    -Updated AmrapRoundBloc, added default for finished
    -Added deactive event, for trigger deafult value for AmrapRoundBloc, on finished or go back events
    -Improve logic for BlocBuilder to update only on value changes

--- lib/blocs/amrap_round_bloc.dart
+++ lib/blocs/amrap_round_bloc.dart
@@ -1,25 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AmrapRound{
+class AmrapRound {
   int amrapValue;
   AmrapRound({
     this.amrapValue,
   });
 }
 
-class AmrapRoundUpdate extends AmrapRound{
-  int amrapValue;
-  AmrapRoundUpdate({
-    this.amrapValue,
-  });
-}
+const int _defaultStateValue = 0;
 
 class AmrapRoundBloc extends Cubit<AmrapRound> {
-  AmrapRoundBloc() : super(AmrapRound(amrapValue: 0));
+  AmrapRoundBloc() : super(AmrapRound(amrapValue: _defaultStateValue));
 
   void set(int value) => emit(AmrapRound(amrapValue: value));
-
   void get() => emit(AmrapRound(amrapValue: state.amrapValue));
-
-  void update() => emit(AmrapRoundUpdate(amrapValue: state.amrapValue));
+  void emitDefault() => emit(AmrapRound(amrapValue: _defaultStateValue));
 }
