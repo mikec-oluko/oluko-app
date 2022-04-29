9c64551ba4a0dba69a73203d19c0edc4540c513a
Victoria Moraes <>
2022-04-29 12:54:19 -0300

    UPDATE: camera bottom sheet in segments to new designs

--- lib/ui/screens/courses/segment_camera_preview.dart.dart
+++ lib/ui/screens/courses/segment_camera_preview.dart.dart
@@ -22,14 +22,7 @@ class SegmentCameraPreview extends StatefulWidget {
   final int courseIndex;
   final UserResponse coach;
 
-  SegmentCameraPreview(
-      {Key key,
-      this.coach,
-      this.courseIndex,
-      this.classIndex,
-      this.segmentIndex,
-      this.courseEnrollment,
-      this.segments})
+  SegmentCameraPreview({Key key, this.coach, this.courseIndex, this.classIndex, this.segmentIndex, this.courseEnrollment, this.segments})
       : super(key: key);
 
   @override
@@ -75,98 +68,78 @@ class _State extends State<SegmentCameraPreview> {
     return Form(
         key: _formKey,
         child: Scaffold(
-            body: Container(
-                color: Colors.black,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    children: [
-                      ConstrainedBox(
-                          constraints: BoxConstraints(
-                              maxHeight: MediaQuery.of(context).size.height),
-                          child: (!_isReady)
-                              ? Container()
-                              : Stack(alignment: Alignment.topRight, children: [
-                                  AspectRatio(
-                                      aspectRatio: 3.0 / 4.0,
-                                      child: CameraPreview(cameraController)),
-                                  Padding(
-                                      padding: const EdgeInsets.only(
-                                          right: 10, top: 15),
-                                      child: Stack(
-                                          alignment: Alignment.center,
-                                          children: [
-                                            Image.asset(
-                                              'assets/courses/grey_circle.png',
-                                              scale: 4,
-                                            ),
-                                            IconButton(
-                                              icon: const Icon(
-                                                Icons.close,
-                                                size: 28,
-                                                color: Colors.grey,
-                                              ),
-                                              onPressed: () =>
-                                                  Navigator.pop(context),
-                                            )
-                                          ])),
-                                ])),
-                      Expanded(
-                          child: Container(
-                              width: ScreenUtils.width(context),
-                              decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/courses/dialog_background.png'),
-                                fit: BoxFit.cover,
-                              )),
-                              child: Column(children: [
-                                Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 20.0, vertical: 4),
-                                    child: Text(
-                                        OlukoLocalizations.of(context)
-                                            .find('cameraInfo'),
-                                        textAlign: TextAlign.center,
-                                        style: OlukoFonts.olukoBigFont(
-                                            custoFontWeight: FontWeight.w300,
-                                            customColor: OlukoColors.white))),
-                                startButton(),
-                                /*Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2),
-                                    child: Text(OlukoLocalizations.of(context).find('cameraWarning'),
-                                        textAlign: TextAlign.center,
-                                        style: OlukoFonts.olukoBigFont(custoFontWeight: FontWeight.w300, customColor: OlukoColors.primary)))*/
-                              ]))),
-                    ],
-                  ),
-                ))));
+          bottomSheet: bottomPanel(),
+          body: Stack(
+            children: [
+              cameraSection(),
+            ],
+          ),
+        ));
+  }
+
+  Widget cameraSection() {
+    return (!_isReady)
+        ? Container()
+        : Stack(alignment: Alignment.topRight, children: [
+            AspectRatio(aspectRatio: 3.0 / 4.0, child: CameraPreview(cameraController)),
+            closeButton(),
+          ]);
+  }
+
+  Widget closeButton() {
+    return Padding(
+        padding: const EdgeInsets.only(right: 10, top: 15),
+        child: Stack(alignment: Alignment.center, children: [
+          Image.asset(
+            'assets/courses/grey_circle.png',
+            scale: 4,
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.close,
+              size: 28,
+              color: Colors.grey,
+            ),
+            onPressed: () => Navigator.pop(context),
+          )
+        ]));
+  }
+
+  Widget bottomPanel() {
+    return Container(
+        height: 230,
+        width: ScreenUtils.width(context),
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            image: DecorationImage(
+              image: AssetImage('assets/courses/dialog_background.png'),
+              fit: BoxFit.cover,
+            )),
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 20),
+            child: Column(children: [
+              Text(OlukoLocalizations.of(context).find('cameraInfo'),
+                  textAlign: TextAlign.left,
+                  style: OlukoFonts.olukoBigFont(custoFontWeight: FontWeight.w600, customColor: OlukoColors.white)),
+              Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child:startButton()),
+              Text(OlukoLocalizations.of(context).find('cameraWarning'),
+                  textAlign: TextAlign.left,
+                  style: OlukoFonts.olukoBigFont(custoFontWeight: FontWeight.w500, customColor: OlukoColors.primary))
+            ])));
   }
 
   Widget startButton() {
     return GestureDetector(
         onTap: () {
-          TimerUtils.startCountdown(
-              WorkoutType.segmentWithRecording,
-              context,
-              getArguments(),
-              widget.segments[widget.segmentIndex].initialTimer,
-              widget.segments[widget.segmentIndex].rounds,
-              0);
+          TimerUtils.startCountdown(WorkoutType.segmentWithRecording, context, getArguments(),
+              widget.segments[widget.segmentIndex].initialTimer, widget.segments[widget.segmentIndex].rounds, 0);
         },
-        child: Stack(alignment: Alignment.center, children: [
-          Image.asset(
-            'assets/courses/oval.png',
-            scale: 4,
-          ),
-          Image.asset(
-            'assets/courses/green_circle.png',
-            scale: 4,
-          ),
-        ]));
+        child: recordIcon());
   }
 
-  getArguments() {
+  Object getArguments() {
     return {
       'segmentIndex': widget.segmentIndex,
       'classIndex': widget.classIndex,
@@ -177,52 +150,21 @@ class _State extends State<SegmentCameraPreview> {
       'segments': widget.segments,
       'showPanel': _user.showRecordingAlert,
       'onShowAgainPressed': () {
-        BlocProvider.of<RecordingAlertBloc>(context)
-            .updateRecordingAlert(_user);
+        BlocProvider.of<RecordingAlertBloc>(context).updateRecordingAlert(_user);
       }
     };
-  }
-
-  Widget formSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 15.0),
-          child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                'title',
-                style: OlukoFonts.olukoSuperBigFont(
-                    customColor: OlukoColors.grayColor,
-                    custoFontWeight: FontWeight.normal),
-              )),
-        ),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              'description',
-              style: OlukoFonts.olukoSuperBigFont(
-                  customColor: OlukoColors.white,
-                  custoFontWeight: FontWeight.normal),
-            )),
-        const SizedBox(height: 50)
-      ],
-    );
   }
 
   Future<void> _setupCameras() async {
     final int cameraPos = isCameraFront ? 0 : 1;
     try {
-      if (!await PermissionsUtils.permissionsEnabled(
-          DeviceContentFrom.camera)) {
+      if (!await PermissionsUtils.permissionsEnabled(DeviceContentFrom.camera)) {
         Navigator.pop(context);
         PermissionsUtils.showSettingsMessage(context);
         return;
       }
       cameras = await availableCameras();
-      cameraController =
-          CameraController(cameras[cameraPos], ResolutionPreset.medium);
+      cameraController = CameraController(cameras[cameraPos], ResolutionPreset.medium);
       await cameraController.initialize();
     } on CameraException catch (e) {
       return;
@@ -233,63 +175,18 @@ class _State extends State<SegmentCameraPreview> {
     });
   }
 
-  Widget bottomBar() {
-    return BottomAppBar(
-      color: Colors.black,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GestureDetector(
-                onTap: () async {
-                  setState(() {
-                    isCameraFront = !isCameraFront;
-                  });
-                  _setupCameras();
-                },
-                child: Stack(alignment: Alignment.center, children: [
-                  Image.asset(
-                    'assets/assessment/camera.png',
-                    scale: 4,
-                  ),
-                  const Icon(
-                    Icons.cached,
-                    color: OlukoColors.grayColor,
-                    size: 18,
-                  ),
-                ])),
-            GestureDetector(
-              onTap: () {},
-              child: _recording ? recordingIcon() : recordIcon(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget recordingIcon() {
-    return Stack(alignment: Alignment.center, children: [
-      Image.asset(
-        'assets/self_recording/red_ellipse.png',
-        scale: 4,
-      ),
-      Image.asset(
-        'assets/self_recording/white_square.png',
-        scale: 4,
-      ),
-    ]);
-  }
-
   Widget recordIcon() {
     return Stack(alignment: Alignment.center, children: [
       Image.asset(
-        'assets/self_recording/white_ellipse.png',
+        'assets/self_recording/green_elipse_cam.png',
         scale: 4,
       ),
       Image.asset(
-        'assets/self_recording/white_filled_ellipse.png',
+        'assets/self_recording/outlined_circle_cam.png',
+        scale: 4,
+      ),
+      Image.asset(
+        'assets/self_recording/white_circle_cam.png',
         scale: 4,
       ),
     ]);
