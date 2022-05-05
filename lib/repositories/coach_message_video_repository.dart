dad42d1ef338e7f686dfd2870c6f4d2b9b16438a
Facundo.p <57139117+pineapplelab23@users.noreply.github.com>
2022-05-05 10:08:46 -0300

    UPDATED: Added repository for video message

--- lib/repositories/coach_message_video_repository.dart
+++ lib/repositories/coach_message_video_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:global_configuration/global_configuration.dart';
 
 class CoachMessageVideoRepository {
@@ -10,4 +11,16 @@ class CoachMessageVideoRepository {
     firestoreInstance = FirebaseFirestore.instance;
   }
   CoachMessageVideoRepository.test({this.firestoreInstance});
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getStream({@required String userId, @required String coachId}) {
+    Stream<QuerySnapshot<Map<String, dynamic>>> coachMessageVideoStream = FirebaseFirestore.instance
+        .collection('projects')
+        .doc(GlobalConfiguration().getValue('projectId'))
+        .collection('coachAssignments')
+        .doc(userId)
+        .collection('mediaMessages')
+        .where('coach_id', isEqualTo: coachId)
+        .snapshots();
+    return coachMessageVideoStream;
+  }
 }
