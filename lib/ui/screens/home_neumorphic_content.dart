3aba5c5f19beb76fbac5b89142936c205523cd69
jacoboFederico <89927652+jacoboFederico@users.noreply.github.com>
2022-05-27 13:00:40 -0300

    Hotfix enroll buton

--- lib/ui/screens/home_neumorphic_content.dart
+++ lib/ui/screens/home_neumorphic_content.dart
@@ -564,9 +564,12 @@ class _HomeNeumorphicContentState extends State<HomeNeumorphicContent> {
 
   Widget notEnrolledStoriesHeader(bool showStories) {
     if (showStories) {
-      return StoriesHeader(
-        widget.user.uid,
-        maxRadius: 30,
+      return Align(
+        alignment: Alignment.centerLeft,
+        child: StoriesHeader(
+          widget.user.uid,
+          maxRadius: 30,
+        ),
       );
     } else {
       return const SizedBox();
