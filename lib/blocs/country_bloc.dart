84ce4ce0272a9e5fb1c29cc31932e22b02798ed6
jacoboFederico <89927652+jacoboFederico@users.noreply.github.com>
2022-07-27 12:08:07 -0300

    CHORE: states and countries dropdowns

---
+++ lib/blocs/country_bloc.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:oluko_app/models/dto/country.dart';
+import 'package:oluko_app/repositories/country_repository.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+abstract class CountryState {}
+
+class Loading extends CountryState {}
+
+class CountrySuccess extends CountryState {
+  List<Country> countries;
+  CountrySuccess({this.countries});
+}
+
+class CountryFailure extends CountryState {
+  final String exceptionMessage;
+  CountryFailure({this.exceptionMessage});
+}
+
+class CountryBloc extends Cubit<CountryState> {
+  CountryBloc() : super(Loading());
+  List<Country> countries;
+
+  Future<void> getCountriesWithStates(String country) async {
+    try {
+      if (countries != null && countries.isNotEmpty) {
+        if (country != null && country != '') {
+          final countryToUpdateIndex = countries.indexWhere((element) => element.name == country);
+          if (countryToUpdateIndex != -1 &&
+              (countries[countryToUpdateIndex].states == null && countries[countryToUpdateIndex].states.isEmpty)) {
+            countries[countryToUpdateIndex].states = await CountryRepository.getCountryStates(countries[countryToUpdateIndex].id);
+          }
+        }
+      } else {
+        countries = await CountryRepository.getCountries();
+      }
+      emit(CountrySuccess(countries: countries));
+    } catch (e, stackTrace) {
+      await Sentry.captureException(
+        e,
+        stackTrace: stackTrace,
+      );
+      emit(CountryFailure(exceptionMessage: e.toString()));
+    }
+  }
+
+  Future<List<Country>> getStatesForCountry(String countryId) async {
+    try {
+      var countryIndex = countries.indexWhere((item) => item.id == countryId);
+      if (countryIndex != -1) {
+        countries[countryIndex].states = await CountryRepository.getCountryStates(countryId);
+        return countries;
+      } else {
+        return [];
+      }
+    } catch (e, stackTrace) {
+      await Sentry.captureException(
+        e,
+        stackTrace: stackTrace,
+      );
+      rethrow;
+    }
+  }
+
+  void clear() {
+    countries = [];
+  }
+}
---
+++ lib/repositories/country_repository.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:oluko_app/models/assessment.dart';
+import 'package:oluko_app/models/dto/country.dart';
+
+class CountryRepository {
+  FirebaseFirestore firestoreInstance;
+
+  CountryRepository() {
+    firestoreInstance = FirebaseFirestore.instance;
+  }
+
+  CountryRepository.test({FirebaseFirestore firestoreInstance}) {
+    this.firestoreInstance = firestoreInstance;
+  }
+
+  Future<List<Assessment>> getAll() async {
+    QuerySnapshot docRef = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc(GlobalConfiguration().getValue('projectId'))
+        .collection('assessments')
+        .get();
+    List<Assessment> response = [];
+    docRef.docs.forEach((doc) {
+      final Map<String, dynamic> element = doc.data() as Map<String, dynamic>;
+      response.add(Assessment.fromJson(element));
+    });
+    return response;
+  }
+
+  static Future<List<Country>> getCountries({String countryName}) async {
+    final QuerySnapshot docRef = await FirebaseFirestore.instance.collection('countries').orderBy('name').get();
+    final List<Country> countries = [];
+    for (final doc in docRef.docs) {
+      final Map<String, dynamic> countryMap = doc.data() as Map<String, dynamic>;
+      final country = Country.fromJson(countryMap);
+      if (countryName != null && countryName == country.name) {
+        await getCountryStates(country.id);
+      }
+      countries.add(country);
+    }
+    if (countryName == null) {
+      countries[0].states = await getCountryStates(countries[0].id);
+    }
+    return countries;
+  }
+
+  static Future<List<String>> getCountryStates(String countryId) async {
+    final snapshot = await FirebaseFirestore.instance.collection('countries').doc(countryId).collection('states').get();
+    final docs = snapshot.docs;
+    if (docs.isNotEmpty) {
+      final states = docs[0].data()['states'] as List<dynamic>;
+      return states.map((state) => state.toString()).toList();
+    }
+    return [];
+  }
+}
