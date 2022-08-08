37fa129f9cbd61f0d8068c028c69b9f1f87da144
jacoboFederico <89927652+jacoboFederico@users.noreply.github.com>
2022-08-08 12:56:54 -0300

    hotfix

--- lib/models/user_statistics.dart
+++ lib/models/user_statistics.dart
@@ -9,11 +9,11 @@ class UserStatistics {
 
   factory UserStatistics.fromJson(Map<String, dynamic> json) {
     return UserStatistics(
-        completedChallenges: json['completed_challenges'] == null && json['completed_challenges'] is int ? 0 : json['completed_challenges'] as int,
-        completedClasses: json['completed_classes'] == null && json['completed_classes'] is int  ? 0 : json['completed_classes'] as int,
-        completedCourses: json['completed_courses'] == null && json['completed_courses'] is int ? 0 : json['completed_courses'] as int,
-        completedSegments: json['completed_segments'] == null && json['completed_segments'] is int ? 0 : json['completed_segments'] as int,
-        appCompleted: json['app_completed'] == null && json['app_completed'] is int ? 0 : json['app_completed'] as int);
+        completedChallenges: json['completed_challenges'] == null || json['completed_challenges'] is! int ? 0 : json['completed_challenges'] as int,
+        completedClasses: json['completed_classes'] == null || json['completed_classes'] is! int  ? 0 : json['completed_classes'] as int,
+        completedCourses: json['completed_courses'] == null || json['completed_courses'] is! int ? 0 : json['completed_courses'] as int,
+        completedSegments: json['completed_segments'] == null || json['completed_segments'] is! int ? 0 : json['completed_segments'] as int,
+        appCompleted: json['app_completed'] == null || json['app_completed'] is! int ? 0 : json['app_completed'] as int);
   }
 
   Map<String, dynamic> toJson() => {
