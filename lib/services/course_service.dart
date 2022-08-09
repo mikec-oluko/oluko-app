e60882a3a38f557cab356f0f054041fea63a47ce
SantiagoPineapple <96312696+SantiagoPineapple@users.noreply.github.com>
2022-08-09 15:28:27 -0300

    Fix OM-2588

--- lib/services/course_service.dart
+++ lib/services/course_service.dart
@@ -1,20 +1,30 @@
 import 'package:oluko_app/models/class.dart';
 import 'package:oluko_app/models/course.dart';
+import 'package:oluko_app/models/course_enrollment.dart';
+import 'package:oluko_app/models/enrollment_audio.dart';
 import 'package:oluko_app/models/movement.dart';
+import 'package:oluko_app/models/submodels/enrollment_class.dart';
 import 'package:oluko_app/models/submodels/object_submodel.dart';
 
 class CourseService {
-  static List<Class> getCourseClasses(Course course, List<Class> classes) {
+  static List<Class> getCourseClasses(List<Class> classes, {CourseEnrollment courseEnrollment,Course course,}) {
     List<Class> courseClasses = [];
     List<String> ids = [];
     List<Class> sortedClasses = [];
 
-    if (course.classes == null) {
+    if (course == null && courseEnrollment==null) {
       return courseClasses;
     }
-    for (ObjectSubmodel classObj in course.classes) {
-      ids.add(classObj.id);
+    if (courseEnrollment != null) {
+      for (EnrollmentClass classObj in courseEnrollment.classes) {
+        ids.add(classObj.id);
+      }
+    } else {
+      for (ObjectSubmodel classObj in course.classes) {
+        ids.add(classObj.id);
+      }
     }
+
     classes.forEach((classObj) {
       if (ids.contains(classObj.id)) {
         courseClasses.add(classObj);
