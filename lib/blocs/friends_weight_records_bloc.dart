cdc76b54b73a9ae0c45840e26483aaae40ade0c2
SantiagoPineapple <96312696+SantiagoPineapple@users.noreply.github.com>
2023-05-19 11:01:08 -0300

    Mere conflicts

--- lib/blocs/friends_weight_records_bloc.dart
+++ lib/blocs/friends_weight_records_bloc.dart
@@ -6,8 +6,12 @@ import 'package:oluko_app/models/user_response.dart';
 import 'package:oluko_app/models/utils/weight_helper.dart';
 import 'package:oluko_app/models/weight_record.dart';
 import 'package:oluko_app/repositories/course_enrollment_repository.dart';
+import 'package:oluko_app/repositories/friend_repository.dart';
 import 'package:oluko_app/repositories/movement_repository.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+
+import '../models/friend.dart';
+import '../repositories/user_repository.dart';
 
 abstract class FriendWeightRecordState {}
 
@@ -29,9 +33,14 @@ class FriendsWeightRecordsBloc extends Cubit<FriendWeightRecordState> {
   FriendsWeightRecordsBloc() : super(Loading());
   final MovementRepository _movementRepository = MovementRepository();
 
-  void getFriendsWeight({List<UserResponse> friends}) async {
+  void getFriendsWeight({String userId}) async {
+    Friend friendData = await FriendRepository.getUserFriendsByUserId(userId);
+    List<UserResponse> friendList;
+    if (friendData != null) {
+      friendList = await Future.wait(friendData.friends.map((friend) async => UserRepository().getById(friend.id)));
+    }
     Map<UserResponse, List<WeightRecord>> friendResults = {};
-    await Future.wait(friends.map((friend) async {
+    await Future.wait(friendList.map((friend) async {
       List<WeightRecord> recordsForFriend = await _movementRepository.getFriendsRecords(friend.id);
       friendResults[friend] = recordsForFriend;
     }));
