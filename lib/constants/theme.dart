212126bbff638c8c4492730c11918e808f572937
SantiagoPineapple <96312696+SantiagoPineapple@users.noreply.github.com>
2023-05-26 13:10:02 -0300

    changes

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -203,15 +203,16 @@ class OlukoNeumorphism {
 
   static NeumorphicStyle getNeumorphicStyleForBorderContainer() {
     return NeumorphicStyle(
-        border: NeumorphicBorder(width: 7, color: OlukoNeumorphismColors.olukoNeumorphicBackgroundLigth),
-        depth: -15,
-        intensity: 0.5,
-        color: OlukoNeumorphismColors.olukoNeumorphicBackgroundLigth,
-        shape: NeumorphicShape.concave,
-        lightSource: LightSource.lerp(LightSource.top, LightSource.bottomRight, 1),
-        boxShape: NeumorphicBoxShape.rect(),
-        shadowDarkColorEmboss: OlukoNeumorphismColors.olukoNeumorphicBackgroundDark,
-        shadowLightColorEmboss: OlukoNeumorphismColors.olukoNeumorphicBackgroundDark,);
+      border: NeumorphicBorder(width: 2, color: OlukoNeumorphismColors.olukoNeumorphicBackgroundLigth),
+      depth: -15,
+      intensity: 0.7,
+      color: OlukoNeumorphismColors.olukoNeumorphicBackgroundLigth,
+      shape: NeumorphicShape.concave,
+      lightSource: LightSource.lerp(LightSource.top, LightSource.bottomRight, 1),
+      boxShape: NeumorphicBoxShape.rect(),
+      shadowDarkColorEmboss: OlukoNeumorphismColors.olukoNeumorphicBackgroundDark,
+      shadowLightColorEmboss: OlukoNeumorphismColors.olukoNeumorphicBackgroundDark,
+    );
   }
 
   static BoxDecoration boxDecorationForKeyboard() {
@@ -249,8 +250,6 @@ class OlukoNeumorphism {
       1
     ], begin: Alignment.topCenter, end: Alignment.bottomCenter);
   }
-
-  
 }
 
 class OlukoNeumorphismColors {
@@ -353,7 +352,7 @@ class OlukoColors {
 
   static const Color lightSkyblue = Color.fromRGBO(171, 247, 233, 1);
 
-    static const Color strongYellow = Color.fromRGBO(254, 192, 0, 1);
+  static const Color strongYellow = Color.fromRGBO(254, 192, 0, 1);
 
   static const Color coachTabIndicatorColor = Color.fromRGBO(247, 177, 171, 1);
 
@@ -389,7 +388,7 @@ class OlukoFonts {
   static const double olukoSuperBigFontSize = 21.0;
   static const double olukoMediumFontSize = 14.0;
   static const double olukoSmallFontSize = 11.0;
-    static const double olukoSuperSmallFontSize = 8.0;
+  static const double olukoSuperSmallFontSize = 8.0;
   static const double olukoBiggestFontSize = 40.0;
 
   static TextStyle olukoMediumFont({FontWeight customFontWeight, Color customColor, TextDecoration decoration}) {
@@ -441,7 +440,7 @@ class OlukoFonts {
         color: customColor != null ? customColor : OlukoColors.white);
   }
 
-    static TextStyle olukoSuperSmallFont({FontWeight customFontWeight, Color customColor}) {
+  static TextStyle olukoSuperSmallFont({FontWeight customFontWeight, Color customColor}) {
     return TextStyle(
         fontFamily: 'Roboto',
         fontSize: olukoSuperSmallFontSize,
