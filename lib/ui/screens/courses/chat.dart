f00d53d3205201ff7373895335eda4acc28b2837
FelipeOsano1 <felipe@pineapple-lab.com>
2023-06-07 14:34:03 -0300

    app bar image

--- lib/ui/screens/courses/chat.dart
+++ lib/ui/screens/courses/chat.dart
@@ -281,6 +281,7 @@ class _ChatScreenState extends State<ChatScreen> {
             title: widget.courseEnrollment.course.name,
             showTitle: true,
             courseImage: widget.courseEnrollment.course.image,
+            rightPadding: false,
             onPressed: () => {BlocProvider.of<ChatSliderMessagesBloc>(context).listenToMessages(widget.currentUser.id, enrollments: widget.enrollments), Navigator.pop(context)}),
         body: SafeArea(
           child: Column(
